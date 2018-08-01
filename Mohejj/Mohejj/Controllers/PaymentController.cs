727be6195490e6df2c416d1694aaf1bab9d33b54
Mohamed Awadallah <mohamed.awadallah_@outlook.com>
2018-08-01 22:01:03 +0200

    files

---
+++ Mohejj/Mohejj/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mohejj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        [HttpPost("{custId}/{amount}")]
+        public IActionResult Pay(string custId, int amount)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Mohejj/Mohejj/Controllers/QRCodeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using QRCoder;
+
+namespace Mohejj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QRCodeController : ControllerBase
+    {
+        private static IConfiguration _configuration;
+
+        public QRCodeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet("{custId}")]
+        public IActionResult GenerateQrCode(string custId)
+        {
+            string url = "http://www.google.com";
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            // Saving Image to server
+            Response.ContentType = "image/jpeg";
+            string folderpath = Url.Content("~/") + "QrCodes";
+            string fileName = "QRCode_" + custId + ".jpg";
+
+            qrCodeImage.Save(string.Format("~/QrCode/" + custId + ".jpg", "image /jpeg"));
+
+            return Ok();
+        }
+    }
+}
